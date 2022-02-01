bf9d1319d4b0d839ea8bfb72975d69548c157246
sreenivasht88 <sreenivasht1201@gmail.com>
2022-02-01 11:31:36 +0530

    1st commit retry

---
+++ src/demoGitForPush/DemoClassTest.java
@@ -0,0 +1,15 @@
+package demoGitForPush;
+
+import org.testng.annotations.Test;
+
+public class DemoClassTest {
+	
+	@Test
+	public void demo() {
+		System.out.println("hiiii");
+		System.out.println("hello");
+		System.out.println("helloooooooo");
+		System.out.println("conflict");
+	}
+
+}
